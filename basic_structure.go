06a18b6c1350f2e87d8ace0399016ed52728ffe0
nireus1997 <8354882@qq.com>
2018-04-10 11:43:58 +0800

    None

---
+++ basic_structure.go
@@ -0,0 +1,25 @@
+//当前程序包名
+package main
+
+//导入其它包
+import "fmt"
+
+//常量
+const PI = 3.14
+
+//全局变量声明和赋值
+var name = "XinFan"
+
+//一般类型声明
+type newType int
+
+//结构的声明
+type xiaoming struct{}
+
+//接口的声明
+type golang interface{}
+
+//由main函数作为程序入口点启动
+func main(){
+  fmt.Println("Hello World")
+}
